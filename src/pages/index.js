d3f6c86c22af9feadaf340aebc7a4cc41f1eeafb
Ryan-Willpower <surasak.c.work@gmail.com>
2018-12-26 03:32:08 +0700

    [add] components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,31 @@
 import React from 'react'
+import styled from 'styled-components'
 import GlobalLayout from '../components/global-layout'
+//
+import Name from '../components/index/name'
+import Pic from '../components/index/pic'
+import Title from '../components/index/title'
+import Skill from '../components/index/skill'
+
+const Main = styled.div`
+  display: grid;
+  grid-template-columns: repeat(6, 1fr);
+  grid-auto-rows: minmax(100px, auto);
+  grid-template-areas: '. .'
+  'name pic'
+  'title .'
+  '. .'
+  'skill skill';
+`
+
 export default () => (
-  <>
-    <p>Hello, World!</p>
+  <Main>
+    <Name
+      fname="Ryan"
+      lname="Willpower" />
+    <Title text="fullstack developer"/>
+    <Pic />
+    <Skill />
     <GlobalLayout />
-  </>
+  </Main>
 )
